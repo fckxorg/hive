b9bf2f24a4b8744a47c17dba8d3972cf3a084326
makokrjashkin <pd2021006@mipt-client.atp-fivt.org>
2021-12-07 22:36:12 +0000

    fix

--- hivetask4/task4.sql
+++ hivetask4/task4.sql
@@ -9,5 +9,5 @@ SET hive.exec.max.dynamic.partitions=100000;
 SET hive.exec.max.dynamic.partitions.pernode=100000;
 
 SELECT Transform(age)
-using 'python3 udf.py' as ages FROM Users;
+using 'python3 udf.py' as ages FROM Users LIMIT 10;
 
